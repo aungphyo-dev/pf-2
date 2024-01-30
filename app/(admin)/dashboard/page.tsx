0da6ede086b1767313ef42545feaed2225c09ac7
Typle <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-01-30 15:34:07 +0630

    fix: admin dashboard chart

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,15 +1,15 @@
-import AdminBlogsChart from '@/components/admin/admin-blogs-chart';
+import { getBlogPosts } from '@/lib/blog';
 import supabase from '@/lib/supabase';
 import { Suspense } from 'react';
 
 const Dashboard = async () => {
-  const { data: views } = await supabase.from('view_blogs').select();
   const { count: projects } = await supabase
     .from('projects')
     .select('*', { count: 'exact', head: true });
+  const blogs = getBlogPosts();
   return (
     <div>
-      <div className='grid gap-4 md:grid-cols-2 mb-5'>
+      <div className='space-y-5 mb-5'>
         <div className='rounded-xl bg-slate-700 text-slate-200 shadow'>
           <div className='p-6 flex flex-row items-center justify-between space-y-0 pb-2'>
             <h3 className='tracking-tight text-sm font-medium'>
@@ -27,13 +27,10 @@ const Dashboard = async () => {
             <h3 className='tracking-tight text-sm font-medium'>Blogs</h3>
           </div>
           <div className='p-6 pt-0'>
-            <Suspense fallback={<span>Loading...</span>}>
-              <div className='text-2xl font-bold'>{views?.length}</div>
-            </Suspense>
+            <div className='text-2xl font-bold'>{blogs.length}</div>
           </div>
         </div>
       </div>
-      {views ? <AdminBlogsChart data={views} /> : <div>Data not found</div>}
     </div>
   );
 };
