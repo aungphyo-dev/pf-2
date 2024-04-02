3676e1794d7c9ccfdd880710d59916b4e51dd651
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-04-02 18:17:35 +0630

    feat(remove): remove cv form link :fire:

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -7,20 +7,10 @@ const About = () => {
       <p className='mb-5 text-[17px] leading-[30px] tracking-wide text-[#94a3b8]'>
         Hello, I am{' '}
         <span className='font-semibold text-slate-200'>Aung Pyae Phyo</span>, I
-        am 20 years old frontend developer based in Mandalay. I am currently
-        part of the talented team at{' '}
-        <span
-          // target='_blank'
-          // rel='noreferrer'
-          // href='https://mml.com.mm'
-          className='font-semibold text-slate-200'
-        >
-          MGC
-        </span>
-        , where I am honing my skills and contributing to innovative web
-        projects with tech stacks that include Javascript, typescript, React.js,
-        Next.js, Node.js, Express.js, PHP, Laravel, MySql, Postgresql,MongoDB,
-        Firebase and Supabase.
+        am frontend developer based in Mandalay. I am honing my skills and
+        contributing to innovative web projects with tech stacks that include
+        Javascript, typescript, React.js, Next.js, Node.js, Express.js, PHP,
+        Laravel, MySql, Postgresql,MongoDB, Firebase and Supabase.
       </p>
       <p className='mb-5 text-[17px] leading-[30px] tracking-wide text-[#94a3b8]'>
         I am passionate about crafting seamless and captivating online
